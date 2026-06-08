ab822076e5abea86a9a94f638bf734c5642e6e7d
Sakhra-butt <sakhranoor@gmail.com>
2026-06-08 06:37:50 +0500

    add vercel url to cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://resume-analyzer-production-0fa0.up.railway.app'
+    'https://resume-analyzer-production-0fa0.up.railway.app',
+    'https://resume-analyzer-lilac-eta.vercel.app'
   ]
 }));
 
